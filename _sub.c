f940fe6e20da41bcac63a7f87dd91d2689b936be
sachihiroo <saif3antar@gmail.com>
2024-01-09 22:33:05 +0100

    subs the top two elem

---
+++ _sub.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+/**
+ * _sub - subs the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of the opcode.
+ */
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		more_err(8, line_number, "sub");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n - (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
