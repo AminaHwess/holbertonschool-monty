6223df5b63f7a3bd28aa16749d26b0f3de3d1454
amina <amina.1111@yahoo.fr>
2024-01-09 21:43:32 +0100

    main function

--- monty.c
+++ monty.c
@@ -1,5 +1,12 @@
 #include "monty.h"
 
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
+ */
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
