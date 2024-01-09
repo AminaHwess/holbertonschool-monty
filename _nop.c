db3f64df7599d71ec0faffcefdc63750a161fe0c
sachihiroo <saif3antar@gmail.com>
2024-01-09 22:04:34 +0100

    Does Nothing

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - a no-operation function
+ * @line_number: line number
+ * @stack: stack head
+ * Return: void
+*/
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
