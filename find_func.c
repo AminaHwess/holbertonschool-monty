5853450b78cb31d625a963d017215148fc00c6fb
sachihiroo <saif3antar@gmail.com>
2024-01-09 21:29:50 +0100

    find the correct function

---
+++ find_func.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+/**
+ * find_func - find the appropriate function for the opcode
+ * @opcode: opcode
+ * @value: argument of opcode
+ * @ln: line number
+ * Return: void
+ */
+void find_func(char *opcode, char *value, int ln)
+{
+	int i;
+	int m;
+
+	instruction_t func_list[] = {
+		{"push", add_stack},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"add", _add},
+		{"swap", _swap},
+		{"nop", _nop},
+		{NULL, NULL}};
+
+	for (m = 1, i = 0; func_list[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, func_list[i].opcode) == 0)
+		{
+			call_fun(func_list[i].f, opcode, value, ln);
+			m = 0;
+		}
+	}
+	if (m == 1)
+		error(3, ln, opcode);
+}
