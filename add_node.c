0a918cd43fdad4898dad2ca6fe32cceec9162174
amina <amina.1111@yahoo.fr>
2024-01-09 21:48:46 +0100

    add node to the stack

---
+++ add_node.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+stack_t *head = NULL;
+
+/**
+ * add_stack - Adds a node to the stack.
+ * @new_node: Pointer to the new node.
+ * @ln: Interger representing the line number of of the opcode.
+ */
+
+void add_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	head = *new_node;
+	head->next = tmp;
+	tmp->prev = head;
+}
